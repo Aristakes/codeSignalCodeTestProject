105fd8994806d12336e4e0728764453ac588b15a
Aris <arissirkin@gmail.com>
2020-08-12 13:06:01 +0200

    Logic for code signal  example name ArrayReplace

---
+++ src/ArrayReplace.java
@@ -0,0 +1,25 @@
+public class ArrayReplace {
+
+    public static void main(String[] args){
+
+
+
+    }
+
+
+
+   static int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem) {
+
+
+       for (int i = 0; i < inputArray.length; i++) {
+           if(inputArray[i] == elemToReplace){
+               inputArray[i] = substitutionElem;
+           }
+       }
+
+       return  inputArray;
+
+    }
+
+
+}
